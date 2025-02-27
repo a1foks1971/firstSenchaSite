f119c53d8025b04c3d9b0d44c5c977e8433d515d
a1foks1971 <a1foks1971@gmail.com>
2025-02-27 13:07:21 +0200

    add dateDiff

--- classic/src/view/main/Main.js
+++ classic/src/view/main/Main.js
@@ -1,103 +1,16 @@
-/**
- * This class is the main view for the application. It is specified in app.js as the
- * "mainView" property. That setting automatically applies the "viewport"
- * plugin causing this view to become the body element (i.e., the viewport).
- *
- * TODO - Replace this content of this view to suite the needs of your application.
- */
 Ext.define('NewExtApp.view.main.Main', {
-    extend: 'Ext.tab.Panel',
-    xtype: 'app-main',
 
-    requires: [
-        'Ext.plugin.Viewport',
-        'Ext.window.MessageBox',
-
-        'NewExtApp.view.main.MainController',
-        'NewExtApp.view.main.MainModel',
-        'NewExtApp.view.main.List'
-    ],
-
-    controller: 'main',
-    viewModel: 'main',
-
-    ui: 'navigation',
-
-    tabBarHeaderPosition: 1,
-    titleRotation: 0,
-    tabRotation: 0,
-
-    header: {
-        layout: {
-            align: 'stretchmax'
-        },
-        title: {
-            bind: {
-                text: '{name}'
-            },
-            flex: 0
-        },
-        iconCls: 'fa-th-list'
-    },
-
-    tabBar: {
-        flex: 1,
-        layout: {
-            align: 'stretch',
-            overflowHandler: 'none'
-        }
-    },
-
-    responsiveConfig: {
-        tall: {
-            headerPosition: 'top'
-        },
-        wide: {
-            headerPosition: 'left'
-        }
-    },
-
-    defaults: {
-        bodyPadding: 20,
-        tabConfig: {
-            responsiveConfig: {
-                wide: {
-                    iconAlign: 'left',
-                    textAlign: 'left'
-                },
-                tall: {
-                    iconAlign: 'top',
-                    textAlign: 'center',
-                    width: 120
-                }
-            }
-        }
-    },
-
-    items: [{
-        title: 'Home',
-        iconCls: 'fa-home',
-        // The following grid shares a store with the classic version's grid as well!
-        items: [{
-            xtype: 'mainlist'
-        }]
-    }, {
-        title: 'Users',
-        iconCls: 'fa-user',
-        bind: {
-            html: '{loremIpsum}'
-        }
-    }, {
-        title: 'Groups',
-        iconCls: 'fa-users',
-        bind: {
-            html: '{loremIpsum}'
-        }
-    }, {
-        title: 'Settings',
-        iconCls: 'fa-cog',
-        bind: {
-            html: '{loremIpsum}'
-        }
-    }]
 });
+start = new Date('2024-03-10 00:00:00'),
+end = new Date('2024-03-11 00:00:00'),
+end2 = new Date('2024-03-12 00:00:00'),
+end3 = new Date('2024-03-17 00:00:00'),
+
+console.log(Ext.Date.diff(start, end, Ext.Date.MINUTE), ' minutes'),
+console.log(Ext.Date.diff(start, end, Ext.Date.HOUR), ' hours'),
+console.log(Ext.Date.diff(start, end, Ext.Date.DAY), ' days'),
+
+console.log(Ext.Date.diff(start, end2, Ext.Date.DAY), ' days'),
+
+console.log(Ext.Date.diff(start, end,  Ext.Date.WEEK), ' weeks'),
+console.log(Ext.Date.diff(start, end3, Ext.Date.WEEK), ' weeks')
--- modern/src/view/main/Main.js
+++ modern/src/view/main/Main.js
@@ -1,56 +1,22 @@
-/**
- * This class is the main view for the application. It is specified in app.js as the
- * "mainView" property. That setting causes an instance of this class to be created and
- * added to the Viewport container.
- */
 Ext.define('NewExtApp.view.main.Main', {
-    extend: 'Ext.tab.Panel',
-    xtype: 'app-main',
-
-    requires: [
-        'Ext.MessageBox',
-        'Ext.layout.Fit'
-    ],
-
-    controller: 'main',
-    viewModel: 'main',
-
-    defaults: {
-        tab: {
-            iconAlign: 'top'
-        }
-    },
-
-    tabBarPosition: 'bottom',
-
-    items: [
-        // TODO - Replace the content of this view to suit the needs of your application.
-        {
-            title: 'Home',
-            iconCls: 'x-fa fa-home',
-            layout: 'fit',
-            // The following grid shares a store with the classic version's grid as well!
-            items: [{
-                xtype: 'mainlist'
-            }]
-        },{
-            title: 'Users',
-            iconCls: 'x-fa fa-user',
-            bind: {
-                html: '{loremIpsum}'
+    extend: 'Ext.Container',
+    padding: 10,
+            items: {
+                xtype: 'button',
+                text: 'My Button',
+                badgeText: '2',
+                handler: function () {
+                    var start = new Date('2024-03-10 00:00:00'),
+                        end = new Date('2024-03-11 00:00:00'),
+                        end2 = new Date('2024-03-12 00:00:00'),
+                        end3 = new Date('2024-03-17 00:00:00');
+    
+                console.log(Ext.Date.diff(start, end, Ext.Date.MINUTE), ' minutes');
+                console.log(Ext.Date.diff(start, end, Ext.Date.HOUR), ' hours');
+                console.log(Ext.Date.diff(start, end, Ext.Date.DAY), ' days');
+                console.log(Ext.Date.diff(start, end2, Ext.Date.DAY), ' days');
+                console.log(Ext.Date.diff(start, end, Ext.Date.WEEK), ' weeks');
+                console.log(Ext.Date.diff(start, end3, Ext.Date.WEEK), ' weeks');
             }
-        },{
-            title: 'Groups',
-            iconCls: 'x-fa fa-users',
-            bind: {
-                html: '{loremIpsum}'
-            }
-        },{
-            title: 'Settings',
-            iconCls: 'x-fa fa-cog',
-            bind: {
-                html: '{loremIpsum}'
-            }
-        }
-    ]
-});
+        }    
+    });
