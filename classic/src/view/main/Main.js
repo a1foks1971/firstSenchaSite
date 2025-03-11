7e0676be5daa3d37249ef3b4ee98e49525fca9d9
a1foks1971 <a1foks1971@gmail.com>
2025-03-11 12:52:24 +0200

    update

--- classic/src/view/main/Main.js
+++ classic/src/view/main/Main.js
@@ -1,13 +1,13 @@
 var store = Ext.create('Ext.data.Store', {
     fields: ['name', 'email', 'phone'],
     data: [
-        { 'name': 'Lisa',  "email":"lisa@simpsons.com",  "phone":"555-111-1224"  },
-        { 'name': 'Bart',  "email":"bart@simpsons.com",  "phone":"555-222-1234" },
-        { 'name': 'Homer', "email":"home@simpsons.com",  "phone":"555-222-1244"  },
-        { 'name': 'Marge', "email":"marge@simpsons.com", "phone":"555-222-1254"  }
+        { 'name': 'Lisa', "email": "lisa@simpsons.com", "phone": "555-111-1224" },
+        { 'name': 'Bart', "email": "bart@simpsons.com", "phone": "555-222-1234" },
+        { 'name': 'Homer', "email": "home@simpsons.com", "phone": "555-222-1244" },
+        { 'name': 'Marge', "email": "marge@simpsons.com", "phone": "555-222-1254" }
     ]
-  });
-   Ext.define('NewExtApp.view.main.Main', {
+});
+Ext.define('NewExtApp.view.main.Main', {
     extend: 'Ext.Container',
     layout: {
         type: 'hbox',
@@ -28,7 +28,7 @@ var store = Ext.create('Ext.data.Store', {
         }
     },
     items: [
-            {
+        {
             title: 'Grid behaviour without configured titleBar',
             defaults: {
                 xtype: 'grid',
@@ -36,44 +36,44 @@ var store = Ext.create('Ext.data.Store', {
                 padding: 2,
                 store: store,
                 padding: "10, 10, 10, 10",
-    columns: [
-        { text: 'Name',  dataIndex: 'name', width: 200 },
-        { text: 'Email', dataIndex: 'email', width: 250 },
-        { text: 'Phone', dataIndex: 'phone', width: 120 }
-    ],
+                columns: [
+                    { text: 'Name', dataIndex: 'name', width: 200 },
+                    { text: 'Email', dataIndex: 'email', width: 250 },
+                    { text: 'Phone', dataIndex: 'phone', width: 120 }
+                ],
                 style: {
                     border: 'solid 1px lightgrey'
                 }
             },
             items: [
                 {
-                 html: 'PASS: NO TITLE - Grid should NOT display titlebar'
-            }
+                    html: 'PASS: NO TITLE - Grid should NOT display titlebar'
+                }
 
-    ],
-    },
-    {
-        title: 'Grid behaviour without configured titleBar',
-        defaults: {
-            xtype: 'grid',
-            flex: 1,
-            padding: 2,
-            store: store,
-            padding: "10, 10, 10, 10",
-columns: [
-    { text: 'Name',  dataIndex: 'name', width: 200 },
-    { text: 'Email', dataIndex: 'email', width: 250 },
-    { text: 'Phone', dataIndex: 'phone', width: 120 }
-],
-            style: {
-                border: 'solid 1px lightgrey'
-            }
+            ],
         },
-        items: [
-            {
-             html: 'PASS: NO TITLE - Grid should NOT display titlebar'
+        {
+            title: 'Grid behaviour without configured titleBar',
+            defaults: {
+                xtype: 'grid',
+                flex: 1,
+                padding: 2,
+                store: store,
+                padding: "10, 10, 10, 10",
+                columns: [
+                    { text: 'Name', dataIndex: 'name', width: 200 },
+                    { text: 'Email', dataIndex: 'email', width: 250 },
+                    { text: 'Phone', dataIndex: 'phone', width: 120 }
+                ],
+                style: {
+                    border: 'solid 1px lightgrey'
+                }
+            },
+            items: [
+                {
+                    html: 'PASS: NO TITLE - Grid should NOT display titlebar'
+                }
+            ],
         }
-    ],
-    }
-]
+    ]
 });
