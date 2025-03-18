e8a47a424c9135bcd3e7a174ee1f8d090a46bbec
a1foks1971 <a1foks1971@gmail.com>
2025-03-18 11:41:21 +0200

    add multiselection

--- modern/src/view/main/Main.js
+++ modern/src/view/main/Main.js
@@ -1,56 +1,65 @@
-/**
- * This class is the main view for the application. It is specified in app.js as the
- * "mainView" property. That setting causes an instance of this class to be created and
- * added to the Viewport container.
- */
-Ext.define('NewExtApp.view.main.Main', {
-    extend: 'Ext.tab.Panel',
-    xtype: 'app-main',
-
-    requires: [
-        'Ext.MessageBox',
-        'Ext.layout.Fit'
+var store = Ext.create('Ext.data.virtual.Store', {
+    fields: [
+        'firstName', 'lastName', 'address', 'company', 'title', {
+            name: 'id',
+            type: 'int'
+        }
     ],
 
-    controller: 'main',
-    viewModel: 'main',
-
-    defaults: {
-        tab: {
-            iconAlign: 'top'
+    proxy: {
+        type: 'ajax',
+        url: 'https://llbzr8dkzl.execute-api.us-east-1.amazonaws.com/production/user',
+        reader: {
+            rootProperty: 'users',
+            totalProperty: 'totalCount'
         }
     },
+    pageSize: 25,
+    autoLoad: true
+});
 
-    tabBarPosition: 'bottom',
 
-    items: [
-        // TODO - Replace the content of this view to suit the needs of your application.
-        {
-            title: 'Home',
-            iconCls: 'x-fa fa-home',
-            layout: 'fit',
-            // The following grid shares a store with the classic version's grid as well!
-            items: [{
-                xtype: 'mainlist'
-            }]
-        },{
-            title: 'Users',
-            iconCls: 'x-fa fa-user',
-            bind: {
-                html: '{loremIpsum}'
-            }
-        },{
-            title: 'Groups',
-            iconCls: 'x-fa fa-users',
-            bind: {
-                html: '{loremIpsum}'
-            }
-        },{
-            title: 'Settings',
-            iconCls: 'x-fa fa-cog',
-            bind: {
-                html: '{loremIpsum}'
-            }
-        }
-    ]
+Ext.define('NewExtApp.view.main.Main', {
+    extend: 'Ext.grid.Grid',
+    xtype: 'infinite-grid',
+    title: 'Infinite Grid',
+
+    // requires: [
+    //     'NewExtApp.view.main.VirtualForum',
+    //     'Ext.grid.filters.Plugin'
+    // ],
+
+    /* plugins: {
+         gridfilters: true
+     },*/
+    store: store,
+    scrollable: true,
+    height: 500,
+    width: 600,
+
+    columns: [{
+        text: 'First Name',
+        width: 150,
+        dataIndex: 'firstName'
+    }, {
+        text: 'Last Name',
+        width: 150,
+        dataIndex: 'lastName'
+    }, {
+        text: 'Id',
+        width: 50,
+        dataIndex: 'id'
+    }, {
+        text: 'Title',
+        flex: 1,
+        dataIndex: 'title'
+    }, {
+        text: 'Address',
+        flex: 1,
+        dataIndex: 'address'
+    }, {
+        text: 'Company',
+        flex: 1,
+        dataIndex: 'company'
+    }]
 });
